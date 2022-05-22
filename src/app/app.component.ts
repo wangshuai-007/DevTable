f01d59224ae04ede72a957f8a5c3b950e68e2ae8
王帅 <wangshuai_007@qq.com>
2022-05-22 09:31:56 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { DxDataGridModule } from 'devextreme-angular';
+
+import { Service, Employee } from './app.service';
+
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
+})
+export class AppComponent {
+  employees: Employee[];
+
+  constructor(service: Service) {
+    this.employees = service.getEmployees();
+  }
+
+  calculateCellValue(data) {
+    return [data.Title, data.FirstName, data.LastName].join(' ');
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    DxDataGridModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
